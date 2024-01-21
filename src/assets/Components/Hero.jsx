fdf6bcb6807638183c5ed2b4357f3f2114e9be9f
Zaajil_Jilu <zaajiljilu@gmail.com>
2024-01-21 17:02:04 +0530

    manhartan changed

--- src/assets/Components/Hero.jsx
+++ src/assets/Components/Hero.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className="bg-[url('herobg.png')] h-screen">
-        <h1 className='text-[44px] pt-83'>Beloved Manhattan soup stand closes</h1>
-        <p>New Yorkers are facing the winter chill...</p>
+        <h1 className='text-[44px] pt-83'>Welcome to Gateway Scholarship</h1>
+        <p>Kerala's Largest Scholarship Platform</p>
     </div>
   )
 }
