f27a29c64ba53b4c8cc2dd3f60fd50100e0f3ac4
Shiyas-N <muhammedshiyasn811@gmail.com>
2024-01-28 12:34:18 +0530

    Fix: Git conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import React from 'react'
-import LandingPage from './assets/LandingPage';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Login from './assets/Components/Login';
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./assets/LandingPage";
 import Login from "./assets/Login";
 import About from "./assets/About";
->>>>>>> 9089d38c6dc65bc40ed6c8cc11af87401a98b1c7
 
 const App = () => {
   return (
--- src/assets/Components/Filter.jsx
+++ src/assets/Components/Filter.jsx
@@ -1,15 +1,14 @@
-<<<<<<< HEAD
-import React from "react";
-
 const Filter = () => {
   return (
     <div className="flex justify-evenly">
-      <img src="bgimage.png" alt="" className="w-[472px] object-cover rounded-[20px]" />
+      <img
+        src="bgimage.png"
+        alt=""
+        className="w-[472px] object-cover rounded-[20px]"
+      />
       <div className="flex flex-col items-center justify-center">
         <div className="flex flex-col items-center justify-center">
-          <h1 className="text-[44px] pt-83">
-            Find scholarships
-          </h1>
+          <h1 className="text-[44px] pt-83">Find scholarships</h1>
           <p className="pl-5 w-3/5 text-pretty">
             Get a collections of suitable scholarships from our expansive
             scholarship database.
@@ -19,7 +18,7 @@ const Filter = () => {
         <form className="max-w-sm mx-auto mt-10">
           <div className="mb-5">
             <label
-              for="class"
+              htmlFor="class"
               className="block mb-2 text-sm font-medium text-gray-900 text-black"
             >
               Your Present Class
@@ -40,14 +39,14 @@ const Filter = () => {
           </div>
           <div className="mb-5">
             <label
-              for="class"
-              class="block mb-2 text-sm font-medium text-gray-900 text-black"
+              htmlFor="class"
+              className="block mb-2 text-sm font-medium text-gray-900 text-black"
             >
               Gender
             </label>
             <select
               id="class"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-light gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-light gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
               <option selected>Select your Gender</option>
               <option value="">Male</option>
@@ -56,22 +55,22 @@ const Filter = () => {
             </select>
           </div>
           <div className="mb-5">
-          <label
-              for="class"
-              class="block mb-2 text-sm font-medium text-gray-900 text-black"
+            <label
+              htmlFor="class"
+              className="block mb-2 text-sm font-medium text-gray-900 text-black"
             >
               State of Institute
             </label>
             <select
               id="class"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-light gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-light gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
               <option selected>State of Institute</option>
               <option value="">Kerala</option>
               <option value="">Outside Kerala</option>
             </select>
           </div>
-          
+
           <button
             type="submit"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -84,10 +83,4 @@ const Filter = () => {
   );
 };
 
-=======
-const Filter = () => {
-  return <div>Filter</div>;
-};
-
->>>>>>> 9089d38c6dc65bc40ed6c8cc11af87401a98b1c7
 export default Filter;
