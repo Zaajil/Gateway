83ebf4bbc1a8a4b772da6d853b4706265285fcbd
ahammedfavask <ahammedkfavas03@gmail.com>
2024-01-31 20:51:23 +0530

    login page

--- src/assets/Login.jsx
+++ src/assets/Login.jsx
@@ -1,5 +1,149 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <div>
+        <section className="bg-white dark:bg-white-900">
+          <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            
+            <img
+              className="w-8 h-8 mr-2"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+              alt="logo"
+            />
+
+            <a
+              href="#"
+              className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-black"
+            >
+              Gateway to Scholarship
+            </a>
+            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0  dark:border-gray-700">
+              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-black md:text-2xl dark:text-black text-center">
+                  Sign in to your account
+                </h1>
+                <form className="space-y-4 md:space-y-6" action="#">
+                  <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" action="#" method="POST">
+                      <div>
+                        <label
+                          htmlFor="email"
+                          className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                          Email address
+                        </label>
+                        <div className="mt-2">
+                          <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            autoComplete="email"
+                            placeholder="  name@gmail.com"
+                            required
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          />
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="  ••••••••"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+
+                  <div className="flex items-start">
+                    <div className="flex items-start">
+                      <div className="flex items-center h-5">
+                        <input
+                          id="remember"
+                          aria-describedby="remember"
+                          type="checkbox"
+                          className="bg-gray-50 border border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                          required
+                        />
+                      </div>
+                      <div className="text-sm ml-3">
+                        <label
+                          htmlFor="remember"
+                          className="font-medium text-black "
+                        >
+                          Remember me
+                        </label>
+                      </div>
+                    </div>
+                    <a
+                      href="#"
+                      className="text-sm text-blue-700 hover:underline ml-auto dark:text-blue-1000"
+                    >
+                      Forget Password?
+                    </a>
+                  </div>
+
+                  <div className="flex items-center justify-center mt-4">
+                    <button
+                      type="button"
+                      className="flex items-center justify-center w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-google mr-2"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z" />
+                      </svg>
+                      Sign in with Google
+                    </button>
+                  </div>
+
+                  <div className="relative flex items-center justify-center w-full mt-6 border border-t">
+                    <div className="absolute px-5 bg-white">Or</div>
+                  </div>
+
+                  <div className="flex items-center justify-between mt-4">
+                    {/* Add Sign In */}
+                    <button
+                      type="submit"
+                      className="flex items-center justify-center  w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                    >
+                      Sign in
+                    </button>
+                  </div>
+
+                  <div className="flex items-center justify-center  w-full">
+                    <p className="text-sm  text-black">
+                      Don't have an account yet?{" "}
+                      <a
+                        href="#"
+                        className="font-medium text-blue-1000 hover:underline dark:text-blue-500"
+                      >
+                        Sign up
+                      </a>
+                    </p>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
